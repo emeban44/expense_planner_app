1832b074f17b09404928a8773d910e22872ed692
emeban44 <emi.cazorla@hotmail.com>
2021-03-09 16:38:52 +0100

    no transactions display added

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    Transaction(
+/*    Transaction(
       id: 't1',
       title: 'New Shoes',
       amount: 69.99,
@@ -53,7 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
       title: 'Weekly Groceries',
       amount: 16.53,
       date: DateTime.now(),
-    ),
+    ), */
   ];
 
   void _addNewTransaction(String txTitle, double txAmount) {
