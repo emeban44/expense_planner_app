f2247aa81cf2729d4d7f28bb829417c316138313
emeban44 <emi.cazorla@hotmail.com>
2021-03-11 17:03:20 +0100

    delete button text added for wider screens

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -53,11 +53,18 @@ class TransactionList extends StatelessWidget {
                   ),
                   subtitle:
                       Text(DateFormat.yMMMd().format(transactions[index].date)),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                    onPressed: () => deleteTx(transactions[index].id),
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 460
+                      ? FlatButton.icon(
+                          textColor: Colors.red,
+                          icon: Icon(Icons.delete),
+                          label: Text("Delete"),
+                          onPressed: () => deleteTx(transactions[index].id),
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () => deleteTx(transactions[index].id),
+                        ),
                 ),
               );
             },
